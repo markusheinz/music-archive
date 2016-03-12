30ed8678ac1b82ac7780506445980767c74fd8dd
markusheinz <markusheinz@users.noreply.github.com>
2016-03-12 15:46:44 +0100

    indentation adjusted, use transactions for read access to data

--- php/getjson.php
+++ php/getjson.php
@@ -2,7 +2,7 @@
 /*
  * Open Source Music Collection Database (working title)
  *
- * (c) 2015 Markus Heinz
+ * (c) 2015, 2016 Markus Heinz
  * 
  * Licensed under the GPL v3.0
  */
@@ -18,91 +18,116 @@ if (isset($_GET['cmd'])) {
 
   switch ($_GET['cmd']) {
 
-  case 'album_list':
-    if (isset($_GET['start']) && isset($_GET['limit'])) {
+    case 'album_list':
+      if (isset($_GET['start']) && isset($_GET['limit'])) {
 
-      $filters = array();
-      $filterKeys = array('artist_id', 'genre_id', 'location_id', 'year', 
-                          'original', 'song');
+        $filters = array();
+        $filterKeys = array('artist_id', 'genre_id', 'location_id', 'year', 
+                            'original', 'song');
 
-      foreach ($filterKeys as $key) {
-        if (isset($_GET[$key])) {
+        foreach ($filterKeys as $key) {
+          if (isset($_GET[$key])) {
 
-          if ($key === 'song') {
+            if ($key === 'song') {
 
-            $filters[$key] = $_GET[$key];
+              $filters[$key] = $_GET[$key];
 
-          } else {
+            } else {
 
-            $filters[$key] = (int) $_GET[$key];
+              $filters[$key] = (int) $_GET[$key];
 
-            if (strlen($_GET[$key]) == 0) { // year may be unspecified
-              $filters[$key] = -1; 
+              if (strlen($_GET[$key]) == 0) { // year may be unspecified
+                $filters[$key] = -1; 
+              }
+
             }
-
           }
         }
+        
+        if ($archive->beginTransaction()) {
+          $albums = $archive->getAlbums($_GET['start'], $_GET['limit'], 
+                                        $filters);
+          HtmlSanitizer::sanitize($albums);
+          $result = new JsonResult($albums, $archive->getAlbumCount($filters));
+        
+          $archive->commitTransaction();
+        }
       }
-      
-      $albums = $archive->getAlbums($_GET['start'], $_GET['limit'], $filters);
-      HtmlSanitizer::sanitize($albums);
-      $result = new JsonResult($albums, $archive->getAlbumCount($filters));
-    }
-    break;
+      break;
 
-  case 'album_detail':
-    if (isset($_GET['id']) && isset($_GET['start']) && 
-        isset($_GET['limit'])) {
-      
-      $songs = $archive->getAlbumSongs($_GET['id'], $_GET['start'], 
-                                       $_GET['limit']);
+    case 'album_detail':
+      if (isset($_GET['id']) && isset($_GET['start']) && 
+          isset($_GET['limit'])) {
+        
+        if ($archive->beginTransaction()) {
+          $songs = $archive->getAlbumSongs($_GET['id'], $_GET['start'], 
+                                           $_GET['limit']);
 
-      HtmlSanitizer::sanitize($songs);
-      $result = new JsonResult($songs, 
-                               $archive->getAlbumSongCount($_GET['id']));
-    }
-    break;
-
-  case 'album_edit':
-    if (isset($_GET['id'])) {
-      
-      $album = $archive->getAlbum($_GET['id']);
-
-      if ($album) {
-        HtmlSanitizer::sanitize($album);
-        $result = new JsonResult($album, 1);
-      } else {
-        $result = new JsonResult(new stdClass, 0);
+          HtmlSanitizer::sanitize($songs);
+          $result = new JsonResult($songs, 
+                                   $archive->getAlbumSongCount($_GET['id']));
+                                   
+          $archive->commitTransaction();
+        }
       }
-    }
-    break;
+      break;
 
-  case 'genre_list':
-    $genres = $archive->getGenres();
-    HtmlSanitizer::sanitize($genres);
+    case 'album_edit':
+      if (isset($_GET['id'])) {
+        
+        $album = $archive->getAlbum($_GET['id']);
 
-    $result = new JsonResult($genres, $archive->getGenreCount());
-    break;
+        if ($album) {
+          HtmlSanitizer::sanitize($album);
+          $result = new JsonResult($album, 1);
+        } else {
+          $result = new JsonResult(new stdClass, 0);
+        }
+      }
+      break;
 
-  case 'artist_list':
-    $artists = $archive->getArtists();
-    HtmlSanitizer::sanitize($artists);
-   
-    $result = new JsonResult($artists, $archive->getArtistCount());
-    break;
+    case 'genre_list':
+      if ($archive->beginTransaction()) {
+        $genres = $archive->getGenres();
+        HtmlSanitizer::sanitize($genres);
+
+        $result = new JsonResult($genres, $archive->getGenreCount());
+        
+        $archive->commitTransaction();
+      }
+      break;
+
+    case 'artist_list':
+      if ($archive->beginTransaction()) {
+        $artists = $archive->getArtists();
+        HtmlSanitizer::sanitize($artists);
+       
+        $result = new JsonResult($artists, $archive->getArtistCount());
+        
+        $archive->commitTransaction();
+      } 
+      break;
 
     case 'location_list':
-      $locations = $archive->getLocations();
-      HtmlSanitizer::sanitize($locations);
+      if ($archive->beginTransaction()) {
+        $locations = $archive->getLocations();
+        HtmlSanitizer::sanitize($locations);
 
-      $result = new JsonResult($locations, $archive->getLocationCount());
+        $result = new JsonResult($locations, $archive->getLocationCount());
+        
+        $archive->commitTransaction();
+      }
     break;
 
     case 'year_list':
-      $years = $archive->getYears();
-      HtmlSanitizer::sanitize($years);
+      if ($archive->beginTransaction()) {
+        $years = $archive->getYears();
+        HtmlSanitizer::sanitize($years);
 
-      $result = new JsonResult($years, $archive->getYearCount());
+        $result = new JsonResult($years, $archive->getYearCount());
+        
+        $archive->commitTransaction();
+      }
     break;
   }
 
